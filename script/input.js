29999f44ae1a4eec6fb9973dd60af866c24052b6
maleneivy <68667843+maleneivy@users.noreply.github.com>
2022-05-25 13:39:22 +0200

    updated html, css and script
    minor issues

--- script/input.js
+++ script/input.js
@@ -1,15 +1,12 @@
 const form = document.querySelector("#contactForm"); //pick the form by its id
-const firstName = document.querySelector("#firstName");
+const name = document.querySelector("#name");
 const inputError = document.querySelector(".input-error");
-const firstNameError = document.querySelector("#firstNameError");
-const lastName = document.querySelector("#lastName");
-const lastNameError = document.querySelector("#lastNameError");
+const nameError = document.querySelector("#nameError");
 const email = document.querySelector("#email");
 const emailError = document.querySelector("#emailError");
 const message = document.querySelector("#message");
 const messageError = document.querySelector("#messageError");
 const success = document.querySelector("#successMessage");
-const submitButton = document.querySelector("#clearSend");
 
 
 
@@ -27,19 +24,11 @@ function validateForm() {
 
     var isFormValid = true;
 
-    if (checkLength(firstName.value, 1) === true) { //checking length of firstname, (don't need the === but it's better for reading the code.) 
-        firstNameError.style.display = "none";
+    if (checkLength(name.value, 1) === true) { //checking length of firstname, (don't need the === but it's better for reading the code.) 
+        nameError.style.display = "none";
     }
     else {
-        firstNameError.style.display = "block";
-        isFormValid = false;
-    }
-
-    if (checkLength(lastName.value, 1) === true) { //checking length of lastname
-        lastNameError.style.display = "none";
-    }
-    else {
-        lastNameError.style.display = "block";
+        nameError.style.display = "block";
         isFormValid = false;
     }
 
@@ -60,10 +49,8 @@ function validateForm() {
     }
 
     if (isFormValid) {
-        // Do something.
         setTimeout(() => {
-            firstName.value = "";
-            lastName.value = "";
+            name.value = "";
             email.value = "";
             message.value = "";
             success.style.display = "block";
